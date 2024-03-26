67433479b7e71ea8154296ab2c7312cc96963b50
Amaniatiah <amaniatiah@gmail.com>
2024-03-26 14:48:30 +0300

    add sort by date

--- Program.cs
+++ Program.cs
@@ -13,17 +13,10 @@
         get { return quantity; }
         set
         {
-            try
-            {
-                if (value >= 0)
-                    quantity = value;
-                else
-                    throw new ArgumentException("Quantity cannot be negative.");
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+
+            quantity = value;
+
+
 
         }
     }
@@ -36,15 +29,22 @@
 
     public Item(string name, int quantity, DateTime createdDate = default)
     {
-
-
-        if (quantity < 0)
+        try
         {
-            throw new ArgumentException("Quantity cannot be negative.");
+            if (quantity < 0)
+            {
+                throw new ArgumentException("Quantity cannot be negative.");
+            }
+
+            this.name = name;
+            this.quantity = quantity;
+            this.createdDate = createdDate == default ? DateTime.Now : createdDate;
+
         }
-        this.name = name;
-        this.quantity = quantity;
-        this.createdDate = createdDate == default ? DateTime.Now : createdDate;
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
 
 
     }
@@ -57,6 +57,12 @@
 
 public class Store
 {
+    public enum SortOrder
+    {
+        ASC,
+        DESC
+    }
+
     private List<Item> items = new List<Item>();
     private int MaximumCapacity;
 
@@ -72,18 +78,26 @@ public class Store
     public void AddItem(Item item)
     {
         bool isItemExist = items.Any((i) => i.name == item.name);
-        if (isItemExist)
+        try
         {
-            throw new ArgumentException($"Item {item.name} already exists.");
+            if (isItemExist)
+            {
+                throw new ArgumentException($"Item {item.name} already exists.");
+
+            }
+            if (GetCurrentVolume() + item.Quantity > MaximumCapacity)
+            {
+                throw new InvalidOperationException("Maximum Capcity");
+
+            }
+
+            items.Add(item);
+        }
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
 
         }
-        if (GetCurrentVolume() > MaximumCapacity)
-        {
-            throw new InvalidOperationException("Maximum Capcity");
-
-        }
-
-        items.Add(item);
 
 
     }
@@ -99,21 +113,29 @@ public class Store
 
     public void RemoveItemByName(string itemName)
     {
-        Item itemToRemove = items.FirstOrDefault(i => i.name == itemName);
-        if (itemToRemove != null)
+        try
         {
-            items.Remove(itemToRemove);
-            throw new ArgumentException($"Item {itemName} removed successfully");
+            Item itemToRemove = items.FirstOrDefault(i => i.name == itemName);
+            if (itemToRemove != null)
+            {
+                items.Remove(itemToRemove);
+                Console.WriteLine($"Item {itemName} removed.");
 
 
+            }
+            else
+            {
+                throw new ArgumentException($"Item {itemName}  not found.");
+
+            }
         }
-        else
+        catch (ArgumentException e)
         {
-            throw new ArgumentException($"Item '{itemName}' not found.");
-
-
+            Console.WriteLine(e.Message);
         }
+
     }
+
 
     public int GetCurrentVolume()
     {
@@ -123,10 +145,8 @@ public class Store
 
     public Item FindItemByName(string itemName)
     {
-        // find the item 
         Item itemFound = items.FirstOrDefault(i => i.name == itemName);
         return itemFound;
-
 
     }
 
@@ -135,6 +155,23 @@ public class Store
         return items.OrderBy(i => i.name).ToList();
     }
 
+    public List<Item> SortByDate(SortOrder sortOrder)
+    {
+        switch (sortOrder)
+        {
+            case SortOrder.ASC:
+                return items.OrderBy(i => i.CreatedDate).ToList();
+            case SortOrder.DESC:
+                return items.OrderByDescending(i => i.CreatedDate).ToList();
+
+            default:
+                return items.OrderBy(i => i.CreatedDate).ToList();
+        }
+
+
+    }
+
+   
 
 
 
@@ -146,57 +183,77 @@ public class MyClass
     public static void Main(string[] args)
     {
 
-        try
+
+        var waterBottle1 = new Item("Water Bottle 1", 10, new DateTime(2023, 1, 1));
+        var waterBottle2 = new Item("Water Bottle 2", 5, new DateTime(2023, 1, 1));
+        var coffee = new Item("Coffee", 20);
+        var sandwich = new Item("Sandwich", -15);
+        var batteries = new Item("Batteries", 10);
+        var umbrella = new Item("Umbrella", 5);
+        var sunscreen = new Item("Sunscreen", 8);
+
+
+        var store = new Store(100);
+
+
+        store.AddItem(waterBottle1);
+        store.AddItem(waterBottle2);
+        store.AddItem(coffee);
+        store.AddItem(sandwich);
+        store.AddItem(batteries);
+        store.AddItem(umbrella);
+        store.AddItem(sunscreen);
+        store.AddItem(sunscreen);
+
+
+
+
+
+        // store.PrintItems();
+        // Console.WriteLine("--------------------------");
+        //  store.RemoveItemByName("Coffee");
+
+
+        Console.WriteLine($"{store.GetCurrentVolume()}");
+
+        Console.WriteLine(store.FindItemByName("Coffee"));
+
+
+        var collections = store.SortByNameAsc();
+        foreach (var item in collections)
         {
-            var waterBottle1 = new Item("Water Bottle 1", 10, new DateTime(2023, 1, 1));
-            var waterBottle2 = new Item("Water Bottle 2", 5, new DateTime(2023, 1, 1));
-            var coffee = new Item("Coffee", 20);
-            var sandwich = new Item("Sandwich", 15);
-            var batteries = new Item("Batteries", 10);
-            var umbrella = new Item("Umbrella", 5);
-            var sunscreen = new Item("Sunscreen", 8);
-
-            var store = new Store(100);
-
-
-            store.AddItem(waterBottle1);
-            store.AddItem(waterBottle2);
-            store.AddItem(coffee);
-            store.AddItem(sandwich);
-            store.AddItem(batteries);
-            store.AddItem(umbrella);
-            store.AddItem(sunscreen);
-
-
-
-
-            // store.PrintItems();
-            // Console.WriteLine("--------------------------");
-            //  store.RemoveItemByName("Coffee");
-
-
-            Console.WriteLine($"{store.GetCurrentVolume()}");
-
-            Console.WriteLine(store.FindItemByName("Coffee"));
-
-
-            var collections = store.SortByNameAsc();
-            foreach (var item in collections)
-            {
-                Console.WriteLine($"{item}");
-
-            }
+            Console.WriteLine($"{item}");
 
         }
-        catch (ArgumentException e)
+
+        Console.WriteLine("--------------------------");
+
+       
+
+
+        var collectionSortedByDateAsc = store.SortByDate(Store.SortOrder.ASC);
+       
+
+        foreach (var item in collectionSortedByDateAsc)
         {
-            Console.WriteLine(e.Message);
-        }
-        catch (InvalidOperationException e)
-        {
-            Console.WriteLine(e.Message);
+            Console.WriteLine($"{item}");
         }
 
+                Console.WriteLine("--------------------------");
+
+
+         var collectionSortedByDateDesc = store.SortByDate(Store.SortOrder.DESC);
+      
+
+        foreach (var item in collectionSortedByDateDesc)
+        {
+            Console.WriteLine($"{item}");
+        }
+
+
+
+
+      
 
 
 
@@ -205,4 +262,8 @@ public class MyClass
 
 
     }
+
+
+
+
 }
